4c1fc7a83ee455e78e4f7309d5d37d9b6ce9bb95
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2017-08-12 13:45:20 +0800

    catch_then_cache method must be available as an instance method as well

--- lib/catch_cache/cache.rb
+++ lib/catch_cache/cache.rb
@@ -18,6 +18,22 @@ module CatchCache
 
             cache
           end
+
+          def catch_then_cache(redis_key, &block)
+            redis = Redis.new
+            val = redis.get(redis_key)
+
+            # retrieve the cache with redis_key as its key
+            cache = JSON.parse(val.blank? ? "[]" : val)
+
+            if cache.blank?
+              timeline_logs = block.call
+              redis.set(redis_key, timeline_logs.to_json)
+              cache = JSON.parse(redis.get(redis_key))
+            end
+
+            cache
+          end
         end
       end
     end
